3e585c121d49c97e87778fbe360f6e43ffb93832
felipe.de.rodrigues <felipe.de.rodrigues@accenture.com>
2017-12-01 13:02:38 -0300

    Adicionado o compilador do Opex (Flex)

---
+++ opex/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -i 's/{targetPlayerMajorVersion}/10.0/g' $flexlib/flex-config.xml && grep -r -l -e "\"assets" -e "'assets" /Opex_Flex_3_6/* | xargs sed -i "s/assets/\/assets/g"
+
+java -Xmx1024m -version
+
+export _JAVA_OPTIONS=-Xmx1024m
+
+mxmlc -load-config config/config-compiler.xml Opex_Flex_3_6/src/mainBarraTopo.mxml -output output/evoraopex.swf
